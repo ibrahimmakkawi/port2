0b7557c07278e92f273095b865d1949d07d58de2
fayad <abdullah0fayad@gmail.com>
2024-02-10 20:22:44 +0200

    big edit

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 let menuIcon = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
+let contactMe = document.querySelector("#contact-me");
 
 menuIcon.onclick = () => {
   menuIcon.classList.toggle("bx-x");
   navbar.classList.toggle("active");
+};
+
+contactMe.onclick = () => {
+  window.location.href = "https://wa.me/905549569785";
 };
 
 let sections = document.querySelectorAll("section");
@@ -11,14 +16,15 @@ let navLinks = document.querySelectorAll("header nav a");
 
 window.onscroll = () => {
   sections.forEach((sec) => {
+    console.log(sec);
     let top = window.scrollY;
     let offset = sec.offsetTop - 100;
     let height = sec.offsetHeight;
     let id = sec.getAttribute("id");
 
     if (top >= offset && top < offset + height) {
-      navLinks.forEach((links) => {
-        links.classList.remove("active");
+      navLinks.forEach((link) => {
+        link.classList.remove("active");
         document
           .querySelector("header nav a[href*=" + id + "]")
           .classList.add("active");
